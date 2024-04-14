122085902c9d9312881d5c9fb0071a40432913b4
wrongcoder <git@wrongcoder.com>
2024-04-13 21:38:26 -0700

    Sample code runs

--- UnscheduledEvaluation/ModEntry.cs
+++ UnscheduledEvaluation/ModEntry.cs
@@ -7,6 +7,16 @@ using StardewValley;
 
 namespace UnscheduledEvaluation;
 
-public class ModEntry
+public class ModEntry : Mod
 {
+    public override void Entry(IModHelper helper)
+    {
+        helper.Events.Input.ButtonPressed += OnButtonPressed;
+    }
+
+    private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
+    {
+        if (!Context.IsWorldReady) return;
+        Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
+    }
 }
