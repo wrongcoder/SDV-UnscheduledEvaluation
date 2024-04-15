0cb7c12b70dc3d0f9daf2f6afc7af27b1d13fa03
wrongcoder <git@wrongcoder.com>
2024-04-14 23:52:37 -0700

    Finish converting content, refactor

--- UnscheduledEvaluation/ModEntry.cs
+++ UnscheduledEvaluation/ModEntry.cs
@@ -18,29 +18,56 @@ public class ModEntry : Mod
     {
         if (e.NameWithoutLocale.IsEquivalentTo("Strings/Locations")) PatchStringsLocations(e);
         if (e.NameWithoutLocale.IsEquivalentTo("Data/Events/Farm")) PatchDataEventsFarm(e);
+        if (e.NameWithoutLocale.IsEquivalentTo("Data/Events/FarmHouse")) PatchDataEventsFarmHouse(e);
     }
 
     private static void PatchStringsLocations(AssetRequestedEventArgs e)
     {
         e.Edit(asset =>
         {
-            var data = asset.AsDictionary<string, string?>().Data;
+            var data = asset.AsDictionary<string, string>().Data;
             data["Farm_GrandpaNote"] = "Edited in DLL\n\n" + data["Farm_GrandpaNote"];
         });
     }
 
-    private static void PatchDataEventsFarm(AssetRequestedEventArgs e)
+    private void PatchDataEventsFarm(AssetRequestedEventArgs e)
     {
         e.Edit(asset =>
         {
-            var data = asset.AsDictionary<string, string?>().Data;
-            const string showCandleUpdate = "2146991/y 3/H";
-            if (data[showCandleUpdate] != null)
-            {
-                // This event is marked unseen by Event.DefaultCommands.GrandpaEvaluation2
-                data["2146991/e 558291/H"] = data[showCandleUpdate];
-                data[showCandleUpdate] = null;
-            }
+            var data = asset.AsDictionary<string, string>().Data;
+            // Update Grandpa candles on shrine: 2146991
+            // This event is marked unseen by Event.DefaultCommands.GrandpaEvaluation2
+            RenameKey(data, "2146991/y 3/H", "2146991/e 558291/H");
         });
+    }
+
+    private void PatchDataEventsFarmHouse(AssetRequestedEventArgs e)
+    {
+        e.Edit(asset =>
+        {
+            var data = asset.AsDictionary<string, string>().Data;
+            // First Grandpa evaluation: 558291
+            // Refers to two years spent on the farm
+            RenameKey(data, "558291/y 3/H", "558291/e 321777/t 600 620/H");
+            // Subsequent Grandpa evaluations: 558292
+            RenameKey(data, "558292/e 321777/t 600 620/H", "558292/e 321777/t 600 620/e 558291/H");
+            // Diamond placed on shrine marks 321777 as seen
+        });
+    }
+
+    private void RenameKey<TKey, TValue>(IDictionary<TKey, TValue> data, TKey oldKey, TKey newKey)
+    {
+        var oldKeyMissing = !data.ContainsKey(oldKey);
+        var newKeyExists = data.ContainsKey(newKey);
+
+        if (oldKeyMissing || newKeyExists)
+        {
+            var m = $"Skipping rename \"{oldKey}\" (missing={oldKeyMissing}) to \"{newKey}\" (exists={newKeyExists})";
+            Monitor.Log(m, LogLevel.Error);
+            return;
+        }
+
+        data[newKey] = data[oldKey];
+        data.Remove(oldKey);
     }
 }
