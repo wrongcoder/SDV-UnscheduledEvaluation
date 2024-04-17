81241a13ae44d040c76f2760f2f5ddec66b3054e
wrongcoder <git@wrongcoder.com>
2024-04-16 18:41:16 -0700

    Make Grandpa events skippable

--- UnscheduledEvaluation/ModConfig.cs
+++ UnscheduledEvaluation/ModConfig.cs
@@ -4,6 +4,12 @@ public sealed class ModConfig
 {
     public bool EnableAlwaysActiveShrinePatch { get; set; } = true;
 
+    public bool SkippableShrineCandles { get; set; } = false;
+
+    public bool SkippableEvaluation1 { get; set; } = false;
+
+    public bool SkippableEvaluation2 { get; set; } = false;
+
     public string? GrandpaNote { get; set; } =
         "{0}-\n\nWhen you are ready for me, place a diamond on this shrine.\n\n-Grandpa";
 }
--- UnscheduledEvaluation/ModEntry.cs
+++ UnscheduledEvaluation/ModEntry.cs
@@ -56,7 +56,10 @@ public class ModEntry : Mod
             var data = asset.AsDictionary<string, string>().Data;
             // Update Grandpa candles on shrine: 2146991
             // This event is marked unseen by Event.DefaultCommands.GrandpaEvaluation2
-            RenameKey(data, "2146991/y 3/H", "2146991/e 558291/H");
+            const string origShrineCandlesKey = "2146991/y 3/H";
+            const string newShrineCandlesKey = "2146991/e 558291/H";
+            if (_config?.SkippableShrineCandles ?? false) PrependEventCommand(data, origShrineCandlesKey, "skippable");
+            RenameKey(data, origShrineCandlesKey, newShrineCandlesKey);
         });
     }
 
@@ -67,9 +70,15 @@ public class ModEntry : Mod
             var data = asset.AsDictionary<string, string>().Data;
             // First Grandpa evaluation: 558291
             // Refers to two years spent on the farm
-            RenameKey(data, "558291/y 3/H", "558291/e 321777/t 600 620/H");
+            const string origEvaluation1Key = "558291/y 3/H";
+            const string newEvaluation1Key = "558291/e 321777/t 600 620/H";
+            if (_config?.SkippableEvaluation1 ?? false) PrependEventCommand(data, origEvaluation1Key, "skippable");
+            RenameKey(data, origEvaluation1Key, newEvaluation1Key);
             // Subsequent Grandpa evaluations: 558292
-            RenameKey(data, "558292/e 321777/t 600 620/H", "558292/e 321777/t 600 620/e 558291/H");
+            const string origEvaluation2Key = "558292/e 321777/t 600 620/H";
+            const string newEvaluation2Key = "558292/e 321777/t 600 620/e 558291/H";
+            if (_config?.SkippableEvaluation2 ?? false) PrependEventCommand(data, origEvaluation2Key, "skippable");
+            RenameKey(data, origEvaluation2Key, newEvaluation2Key);
             // Diamond placed on shrine marks 321777 as seen
         });
     }
@@ -88,5 +97,19 @@ public class ModEntry : Mod
 
         data[newKey] = data[oldKey];
         data.Remove(oldKey);
+    }
+
+    private void PrependEventCommand(IDictionary<string, string> data, string eventKey, string eventCommand)
+    {
+        if (!data.TryGetValue(eventKey, out var eventScript))
+        {
+            Monitor.Log($"Skipping prepend to missing key \"{eventKey}\"", LogLevel.Error);
+            return;
+        }
+
+        var parts = eventScript.Split('/', 4);
+        var insertIndex = $"{parts[0]}/{parts[1]}/{parts[2]}/".Length;
+        var prependedEventScript = eventScript.Insert(insertIndex, $"{eventCommand}/");
+        data[eventKey] = prependedEventScript;
     }
 }
